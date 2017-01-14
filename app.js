bc5dadc1f0949e39badb36d2d3739811830a0237
Omer Alper Ozkan <omeralper87@hotmail.com>
2017-01-15 00:31:18 +0300

    package.json

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get('/', function (req, res) {
     res.sendFile('index.html',{ root: path.join( __dirname , '..' )});
 });
 
-server.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+server.listen(5000, function () {
+    console.log('Example app listening on port 5000!')
 });
